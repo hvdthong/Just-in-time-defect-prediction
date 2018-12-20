e5f0399e198d445ff1a48b0c4b865a95385361fd
hvdthong <hvdthong@gmail.com>
2018-12-20 09:34:49 +0800

    adding data

--- clean_commit.py
+++ clean_commit.py
@@ -3,22 +3,26 @@ from os.path import isfile, join
 from parser_commit import get_ids, info_commit
 import nltk
 from ultis import load_file
+from nltk.tokenize import word_tokenize
+import string
+from nltk.corpus import stopwords
 
 
 def clean_message(data):
-    new_data = [' '.join(nltk.word_tokenize(d['title'] + ' ' + d['desc'])).lower() for d in data]
-    return new_data
+    return [' '.join(word_tokenize(d['title'] + ' ' + d['desc'])).lower() for d in data]
 
 
-def clean_code_line_openstack(code_line):
-    if not (code_line[1:].startswith('#')):
-        code_line = code_line[1:].replace("'", ' ').replace(',', ' ').replace('=', ' = ').replace('.', ' ') \
-            .replace(':', ' ').replace('"', ' ').replace('[', ' ').replace(']', ' ').replace('(', ' ') \
-            .replace(')', ' ').replace('&', ' & ').replace('"', ' ').replace(':', ' ').replace('{', ' ') \
-            .replace('}', ' ').replace('#', ' ').replace('/', ' ')
-        return nltk.word_tokenize(code_line)
-    else:
-        return ''
+def clean_code_line(line):
+    for p in string.punctuation:
+        if line.startswith('+#') or line.startswith('-#'):
+            line = line[2:].replace(p, ' ' + p + ' ')
+        elif line.startswith('+') or line.startswith('-') or line.startswith('#'):
+            line = line[1:].replace(p, ' ' + p + ' ')
+        elif line == '':
+            pass
+        else:
+            line = line.replace(p, ' ' + p + ' ')
+    return line
 
 
 def clean_code(data, project):
@@ -29,18 +33,10 @@ def clean_code(data, project):
             file_diff = file_['diff']
             lines = list()
             for line in file_diff:
-                if project == 'openstack':
-                    code_line = clean_code_line_openstack(code_line=line)
-                    lines.append(' '.join(code_line))
-                    # if not (line.startswith('+#') or line.startswith('-#')):
-                    #     file_diff = nltk.word_tokenize(line[1:].replace("'", ' ').replace(',', ' '))
-                    #     lines.append(file_diff)
-
+                line = clean_code_line(line=line)
+                lines.append(' '.join(word_tokenize(line)))
             new_diff.append(' '.join(lines).strip())
         new_diffs.append(new_diff)
-
-    # new_diffs = [clean_code_line_openstack(line) for line in file_['diff'] for file_ in diff for diff in data if
-    #              project == 'openstack']
     print(len(new_diffs))
     exit()
     return new_diffs
@@ -54,14 +50,21 @@ def collect_labels(path_data, path_label):
     return valid_ids, labels_valid_ids
 
 
+def collect_labels_ver2(path_label):
+    ids, labels = [l.split('\t')[0] for l in load_file(path_file=path_label)], [l.split('\t')[1] for l in
+                                                                                load_file(path_file=path_label)]
+    return ids, labels
+
+
 if __name__ == '__main__':
     project = 'openstack'
     # project = 'qt'
     path_data = './output/' + project
     path_label = './labels/' + project + '_ids_label.txt'
-    ids, labels = collect_labels(path_data=path_data, path_label=path_label)
+
+    # ids, labels = collect_labels(path_data=path_data, path_label=path_label)
+    ids, labels = collect_labels_ver2(path_label=path_label)
 
     messages, codes = info_commit(ids=ids, path_file=path_data)
     print(len(ids), len(labels), len(messages), len(codes))
-
-    messages, codes = clean_message(data=messages), clean_code(data=codes, project=project)
+    messages, codes = clean_message(data=messages[:500]), clean_code(data=codes[:500], project=project)
