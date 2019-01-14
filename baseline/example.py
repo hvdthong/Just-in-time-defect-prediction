639c9562821a14b63128812bae2673599a0d037e
hvdthong <hvdthong@gmail.com>
2019-01-14 17:12:02 +0800

    files

--- baseline/example.py
+++ baseline/example.py
@@ -1,11 +1,11 @@
 from load_data import loading_variable_path
-from jit_approach.split_train_test import convert_label
+from split_train_test import convert_label
 
 
 if __name__ == '__main__':
     project = 'openstack'
-    ids, labels = loading_variable_path(pname='../variables/' + project + '_ids.pkl'), convert_label(
-        loading_variable_path(pname='../variables/' + project + '_labels.pkl'))
+    ids, labels = loading_variable_path(pname='../variables_ver1/' + project + '_ids.pkl'), convert_label(
+        loading_variable_path(pname='../variables_ver1/' + project + '_labels.pkl'))
     print(len(ids), len(labels))
     for i, l in zip(ids, labels):
         if l == 1:
---
+++ clean_commit_authordate.py
@@ -0,0 +1,35 @@
+from baseline.load_data import loading_variable_path, convert_label
+import pandas as pd
+from parser_commit import info_commit
+from clean_commit import clean_message, clean_code, saving_variable
+
+
+def sorted_authordate(ids, path_data):
+    # sorted data based on author date and return the ids and labels
+    df = pd.read_csv(path_data)
+    new_df = list()
+    for id in ids:
+        new_df.append(df.loc[df['commit_id'] == id])
+    new_df = pd.concat(new_df)
+    new_df = new_df.sort_values(by=['author_date']).fillna(0)
+    ids, labels = list(new_df['commit_id'].values), list(new_df['bugcount'].values)
+    labels = [0 if (l == 0) else 1 for l in labels]
+    return ids, labels
+
+
+if __name__ == '__main__':
+    project = 'openstack'
+    ids, labels = loading_variable_path(pname='./variables_ver1/' + project + '_ids.pkl'), convert_label(
+        loading_variable_path(pname='./variables_ver1/' + project + '_labels.pkl'))
+    path_data = './labels/' + project + '.csv'
+    ids, lables = sorted_authordate(ids=ids, path_data=path_data)
+
+    path_file = './output/' + project
+    messages, codes = info_commit(ids=ids, path_file=path_file)
+    print(len(ids), len(labels), len(messages), len(codes))
+    messages, codes = clean_message(data=messages), clean_code(data=codes)
+
+    saving_variable(project + '_messages', messages)
+    saving_variable(project + '_codes', codes)
+    saving_variable(project + '_labels', labels)
+    saving_variable(project + '_ids', ids)
