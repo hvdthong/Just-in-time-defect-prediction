49d02edc63906be127a04e53e8058fc01d2835b0
hvdthong <hvdthong@gmail.com>
2019-01-14 11:05:08 +0800

    add files

---
+++ baseline/example.py
@@ -0,0 +1,12 @@
+from load_data import loading_variable_path
+from split_train_test import convert_label
+
+
+if __name__ == '__main__':
+    project = 'openstack'
+    ids, labels = loading_variable_path(pname='../variables/' + project + '_ids.pkl'), convert_label(
+        loading_variable_path(pname='../variables/' + project + '_labels.pkl'))
+    print(len(ids), len(labels))
+    for i, l in zip(ids, labels):
+        if l == 1:
+            print(i, l)
