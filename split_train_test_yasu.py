5fd6c539a4e6f96805f85f870317d28ea45b6d1b
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2019-01-15 15:00:53 +0800

    adding split train/test

--- split_train_test_yasu.py
+++ split_train_test_yasu.py
@@ -18,22 +18,17 @@ def get_label(data):
 
 def load_df_yasu_data(path_data):
     data = pd.read_csv(path_data)
+    data = replace_value_dataframe(df=data)
+    ids, features = get_ids(data=data), get_features(data=data)
+    return (ids, features)
 
 
 def load_yasu_data(project, duration, period):
     if project == 'openstack' and period == 'long':
         train_path_data = './yasu_replication_data/' + project + '.STRATA_PER_YEAR.4.all.7.csv'
-        train_data = pd.read_csv(path_data)
-        data = list()
-        for i in range(0, 5):
-            path_data = './yasu_replication_data/' + project + '.STRATA_PER_YEAR.4.all.' + str(i) + '.csv'
-            data.append(pd.read_csv(path_data))
-        data_new = pd.concat(data)
-        data_new = replace_value_dataframe(df=data_new)
-        ids, features = get_ids(data=data_new), get_features(data=data_new)
-        return ids, features
-
-
+        test_path_data = './yasu_replication_data/' + project + '.STRATA_PER_YEAR.4.local.8.csv'
+        train, test = load_df_yasu_data(train_path_data), load_df_yasu_data(test_path_data)
+        return train, test
 
 
 if __name__ == '__main__':
